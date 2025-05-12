6759c69654360d39c7198d213ab653bb383f6d3b
ΔΙΖΗΣ ΑΓΓΕΛΟΣ <dizisa@interamerican.gr>
2025-05-13 01:51:38 +0300

    gitignore

--- WebKoinoxrhsta/src/main/java/com/angelos/koinoxrhsta/web/MainLayout.java
+++ WebKoinoxrhsta/src/main/java/com/angelos/koinoxrhsta/web/MainLayout.java
@@ -1,15 +1,15 @@
 package com.angelos.koinoxrhsta.web;
 
-import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.login.LoginForm;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.router.Route;
 
 @Route("")
 public class MainLayout extends HorizontalLayout {
 
-    MainLayout(){
-        Button newBuildingButton = new Button("Νέο κτήριο");
-        add(newBuildingButton);
+    MainLayout() {
+        LoginForm loginForm = new LoginForm();
+        add(loginForm);
     }
 
 }
